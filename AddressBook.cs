a3c2d36b069452018c4f062d0de8335fa11c7bcf
SUDHEER341 <sudheer601220@gmail.com>
2023-06-16 22:49:29 +0530

    Added : Added cutom exception and system exception to the address book application

---
+++ AddressBook.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookException
+{
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+     class AddressBook
+    {
+        private const int MaxContacts = 10;
+        private Contact[] contacts;
+        public int contactCount;
+
+        public AddressBook()
+        {
+            contacts = new Contact[MaxContacts];
+            contactCount = 0;
+        }
+
+        public void AddContact(Contact contact)
+        {
+
+             if (contactCount < MaxContacts)
+            {
+                contacts[contactCount] = contact;
+                contactCount++;
+            }
+            else
+            {
+                Console.WriteLine("Address book is full. Cannot add more contacts.");
+            }
+        }
+
+         public void EditContact()
+             {
+            Console.WriteLine("Enter the name of the contact to edit:");
+            string editName = Console.ReadLine();
+
+
+            bool isFound = false;
+            for (int i = 0; i < contactCount; i++)
+            {
+                if (contacts[i].FirstName.Equals(editName, StringComparison.OrdinalIgnoreCase))
+                {
+                   
+                    Console.WriteLine("Enter LastName:");
+                    contacts[i].LastName = Console.ReadLine();
+                    Console.WriteLine("Enter ContactNumber:");
+                    contacts[i].ContactNumber = Console.ReadLine();
+                    Console.WriteLine("Enter Email:");
+                    contacts[i].Email = Console.ReadLine();
+                    Console.WriteLine("Enter City:");
+                    contacts[i].City = Console.ReadLine();
+                    Console.WriteLine("Enter State:");
+                    contacts[i].State = Console.ReadLine();
+                    Console.WriteLine("Enter Zip Code:");
+                    contacts[i].ZipCode = Console.ReadLine();
+
+                    Console.WriteLine("Contact updated successfully!");
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+        public void DeleteContact(string contactName)
+        {
+            int index = -1;
+
+            for (int i = 0; i < contactCount; i++)
+            {
+                if (contacts[i].FirstName.Equals(contactName, StringComparison.OrdinalIgnoreCase))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+            {
+                for (int i = index; i < contactCount - 1; i++)
+                {
+                    contacts[i] = contacts[i + 1];
+                }
+                contactCount--;
+
+                Console.WriteLine("Contact deleted successfully.");
+            }
+
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+
+        }
+
+        public void PrintContacts()
+        {
+            if (contactCount != 0) {
+                for (int i = 0; i < contactCount; i++)
+                {
+                    Console.WriteLine($"Contact {i + 1}:");
+                    Console.WriteLine($"FirstName: {contacts[i].FirstName}");
+                    Console.WriteLine($"LastName: {contacts[i].LastName}");
+                    Console.WriteLine($"ContactNumber: {contacts[i].ContactNumber}");
+                    Console.WriteLine($"Email: {contacts[i].Email}");
+                    Console.WriteLine($"City: {contacts[i].City}");
+                    Console.WriteLine($"State: {contacts[i].State}");
+                    Console.WriteLine($"ZipCode: {contacts[i].ZipCode}");
+
+                    Console.WriteLine();
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No contacts in address book");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System.Linq.Expressions;
+namespace AddressBookException
+{
+
+    public class InvalidInput : Exception
+    {
+        public InvalidInput() { }
+        public InvalidInput(string message) : base(message)
+        {
+
+        }
+    }
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+
+            while (true)
+            {
+                Console.WriteLine("Address Book Menu:");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Edit Contact");
+                Console.WriteLine("3. Delete Contact");
+                Console.WriteLine("4. Print Contacts");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Enter your choice:");
+                try
+                {
+                    int choice = int.Parse(Console.ReadLine());
+
+                    if (choice > 5)
+                    {
+                        throw new InvalidInput();
+
+                    }
+
+
+
+                    switch (choice)
+                    {
+
+
+                        case 1:
+                            Console.WriteLine("Enter First Name:");
+                            string FirstName = Console.ReadLine();
+                            Console.WriteLine("Enter LastName:");
+                            string LastName = Console.ReadLine();
+                            Console.WriteLine("Enter Contact Number:");
+                            string ContactNumber = Console.ReadLine();
+                            Console.WriteLine("Enter Email ID:");
+                            string Email = Console.ReadLine();
+                            Console.WriteLine("Enter Your city:");
+                            string City = Console.ReadLine();
+                            Console.WriteLine("Enter Your state :");
+                            string State = Console.ReadLine();
+                            Console.WriteLine("Enter Your Zip code:");
+                            string ZipCode = Console.ReadLine();
+
+
+                            Contact contact = new Contact
+                            {
+                                FirstName = FirstName,
+                                LastName = LastName,
+                                ContactNumber = ContactNumber,
+                                Email = Email,
+                                City = City,
+                                State = State,
+                                ZipCode = ZipCode
+
+                            };
+
+                            addressBook.AddContact(contact);
+                            Console.WriteLine("Contact added successfully.");
+                            Console.WriteLine();
+                            break;
+
+                        
+
+                        case 2: 
+                            addressBook.EditContact(); 
+                        break;
+                            
+                        case 3:
+                            Console.WriteLine("enter the first name to delete the contact");
+                            string deleteFirstName = Console.ReadLine();
+                            addressBook.DeleteContact(deleteFirstName);
+                            break;
+
+                        case 4:
+                            addressBook.PrintContacts();
+                            Console.WriteLine();
+                            break;
+
+                        case 5:
+                            return;
+
+
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            Console.WriteLine();
+                            break;
+                    }
+
+                }
+
+                //custom exception
+
+                catch (InvalidInput)
+                {
+                    Console.WriteLine("custom exception :  Invalid input please enter the valid input from the MENU[1-5]");
+                }
+
+                //system exception 
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("PLEASE ENTER ONLY NUMBERES");
+                }
+            }
+
+        }
+    }
+}
